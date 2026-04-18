67e7770f4dd737da904a8811f684bd0b7c64b584
Khadija Rafiq <your_github_email@example.com>
2026-04-18 10:00:01 +0500

    Improved navbar layout, smooth scrolling, update projects and correct typography in the footer

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,8 +10,10 @@ const Hero = () => {
         <h3>Frontend Developer (React.js).</h3>
         <h4>I build modern and responsive React websites designed for performance and usability.</h4>
         <div className="hero-buttons">
-          <button className="btn hero-hire-btn">Hire Me</button>
-          <button className="btn hero-project-btn">Projects</button>
+          <a href="mailto:khadija62.dev@email.com">
+          <button className="btn hero-hire-btn">Hire Me</button></a>
+          <a href="#projects">
+          <button className="btn hero-project-btn">Projects</button></a>
         </div>
       </div>
     </div>
